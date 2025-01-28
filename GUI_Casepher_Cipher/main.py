68ed49deb42036da81cd18861ba3bd57dc2c4bc5
Het Patel <hetj1109@gmail.com>
2025-01-28 17:56:56 +0530

    Update and rename main.py to GUI_Casepher_Cipher/main.py

--- GUI_Casepher_Cipher/main.py
+++ GUI_Casepher_Cipher/main.py
@@ -1,8 +1,6 @@
 #
-#
+#Normal code without gui
 # alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-#
-#
 #
 # def caesar(original_text, shift_amount, encode_or_decode):
 #     output_text = ""
@@ -17,15 +15,13 @@
 #     print(f"Here is the {encode_or_decode}d result: {output_text}")
 #
 #
-#
-#
 # direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 # text = input("Type your message:\n").lower()
 # shift = int(input("Type the shift number:\n"))
 #
+#
+#
 # caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
-#
-#
 #
 
 
